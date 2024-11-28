b1fec06ff11c1c1c574b98c1382227b965a10b5a
Aarav <asm374@cornell.edu>
2024-11-28 11:49:35 -0500

    basic login page

--- GymTrackingApp/GymTrackingApp/ContentView.swift
+++ GymTrackingApp/GymTrackingApp/ContentView.swift
@@ -9,25 +9,58 @@ import SwiftUI
 import UIKit
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         VStack {
             Text("Your Gym Tracker")
                 .bold()
                 .padding(50)
             
-            HStack {
-                Text("Username")
-                
-            }
+            VStack {
+                Group {
+                    Text("Username")
+                        .font(.caption)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.gray)
+                    
+                    TextField("Enter Username Here", text: $username)
+                        .padding()
+                        .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(.black, lineWidth: 1)
+                        )
+                        .padding(.bottom, 30)
+                } // end username group
+                Group {
+                    Text("Password")
+                        .font(.caption)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.gray)
+                    
+                    TextField("Enter Password Here", text: $password)
+                        .padding()
+                        .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(.black, lineWidth: 1)
+                        )
+                } // end password group
+            } // end username-password vstack
+            .padding([.leading, .trailing], 20)
             
-            HStack {
-                Text("Password")
+            Button {
                 
-            }
+            } label: {
+                Text("Log In")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                    .background(.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+            } // end button
             
-        }
-    }
-}
+        } // end main Vstack
+    } // end body
+} // end contentview
 
 #Preview {
     ContentView()
---
+++ GymTrackingApp/GymTrackingApp/NetworkManager.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkManager.swift
+//  GymTrackingApp
+//
+//  Created by Aarav on 11/28/24.
+//
+
+import Alamofire
+import Foundation
+
+public class NetworkManager {
+    
+    public let shared = NetworkManager()
+    
+    private init () {}
+    
+    private let endpoint = ""
+    private let decoder = JSONDecoder()
+    
+    //MARK: add user
+    
+    
+    //MARK: get all users
+    
+    //MARK: get user by ID
+    
+    
+    
+}
